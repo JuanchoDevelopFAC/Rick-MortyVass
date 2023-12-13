cd94e17c16e42c25bcb6f938cdbc974b41a2dabe
JuanFACDev <50719623+JuanchoDevelopFAC@users.noreply.github.com>
2023-12-13 17:35:30 -0500

    Initialized Project
    Created project with the basic configuration

---
+++ Rick-and-Morty-VASS/Rick-and-Morty-VASS/Rick_and_Morty_VASSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_VASSApp.swift
+//  Rick-and-Morty-VASS
+//
+//  Created by Juan Fernando Alvarez Carvajal on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_VASSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
