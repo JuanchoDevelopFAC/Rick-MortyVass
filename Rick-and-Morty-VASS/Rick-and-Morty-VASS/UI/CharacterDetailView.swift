e78d993808ae96d56b19af5f23574c8be3f9adf9
JuanFACDev <50719623+JuanchoDevelopFAC@users.noreply.github.com>
2023-12-15 14:30:45 -0500

    Character detail view implementation
    Character detail view implementation.
    Redesign character item in the list view.

--- Rick-and-Morty-VASS/Rick-and-Morty-VASS/UI/CharacterDetailView.swift
+++ Rick-and-Morty-VASS/Rick-and-Morty-VASS/UI/CharacterDetailView.swift
@@ -8,11 +8,134 @@
 import SwiftUI
 
 struct CharacterDetailView: View {
+    
+    let character: Character
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            
+            VStack(alignment: .center, spacing: 40) {
+                characterImage
+                
+                Text(character.name)
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundStyle(Color(colorName: .blackText))
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.horizontal, 10)
+            }
+            
+            characterInfo
+                .padding(.horizontal, 10)
+        }
+        Spacer()
+    }
+    
+    var characterImage: some View {
+        AsyncImage(url: URL(string: character.image)!) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    .clipped()
+            } else if phase.error != nil {
+                Text(phase.error?.localizedDescription ?? "Error")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 350, height: 350)
+    }
+    
+    var characterInfo: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Status: ")
+                    .font(.system(size: 17, weight: .bold))
+                liveStatus
+            }
+            
+            HStack {
+                Text("Gender: ")
+                    .font(.system(size: 17, weight: .bold))
+                Text(character.gender)
+            }
+            
+            HStack {
+                Text("Species: ")
+                    .font(.system(size: 17, weight: .bold))
+                Text(character.species)
+            }
+            
+            HStack {
+                Text("Episodes: ")
+                    .font(.system(size: 17, weight: .bold))
+                Text("\(character.episode.count)")
+            }
+            
+            HStack {
+                Text("Origin: ")
+                    .font(.system(size: 17, weight: .bold))
+                Text(character.origin.name)
+            }
+            
+            HStack {
+                Text("Location: ")
+                    .font(.system(size: 17, weight: .bold))
+                Text(character.location.name)
+            }
+        }
+    }
+    
+    var liveStatus: some View {
+        switch character.status {
+        case "Alive":
+            return AnyView(
+                Text(character.status)
+                    .font(.system(size: 18, weight: .regular))
+                    .foregroundStyle(Color(colorName: .alive)))
+        case "Dead":
+            return AnyView(Text(character.status)
+                .font(.system(size: 18, weight: .regular))
+                .foregroundStyle(Color(colorName: .dead)))
+        case "unknown":
+            return AnyView(Text(character.status)
+                .font(.system(size: 18, weight: .regular))
+                .foregroundStyle(Color(colorName: .blackText)))
+        default:
+            return AnyView(Text(character.status)
+                .font(.system(size: 18, weight: .regular))
+                .foregroundStyle(Color(colorName: .blackText)))
+        }
     }
 }
 
 #Preview {
-    CharacterDetailView()
+    let origin: OriginCharacter = OriginCharacter(
+        name: "Earth (C-137)",
+        url: "https://rickandmortyapi.com/api/location/1"
+    )
+    
+    let location: LocationCharacter = LocationCharacter(
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3")
+    
+    let character: Character = Character(
+        id: 1,
+        name: "Rick Sanchez",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: origin,
+        location: location,
+        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        episode: ["https://rickandmortyapi.com/api/episode/1",
+                  "https://rickandmortyapi.com/api/episode/2",
+                  "https://rickandmortyapi.com/api/episode/3"],
+        url: "https://rickandmortyapi.com/api/character/1",
+        created: "2017-11-04T18:48:46.250Z")
+    
+    return CharacterDetailView(character: character)
 }
